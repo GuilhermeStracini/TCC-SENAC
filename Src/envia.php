26aa6e44a8b41494c743b70531f153f5e585ee96
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-11-10 23:39:17 +0000

    Update envia.php

--- Src/envia.php
+++ Src/envia.php
@@ -2,25 +2,29 @@
 
 if (!isset($_POST) || !isset($_POST['nome']) || !isset($_POST['email'])) {
 	header('Location: index.html');
-	die();
+	exit();
 }
 
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$encontrou = $_POST['encontrou'];
-$assunto = $_POST['assunto'];
-$mensagem = $_POST['mensagem'];
 
-$msg = "<strong>Nome: </strong>" . $nome . "<br />";
-$msg .= "<strong>E-mail: </strong>" . $email . "<br />";
-$msg .= "<strong>Como encontrou o site: </strong>" . $encontrou . "<br />";
-$msg .= "<strong>Assunto: </strong>" . $assunto . "<br />";
-$msg .= "<strong>Mensagem: </strong>" . $mensagem . "<br />";
+$postfilter = array(
+	'nome' => FILTER_SANITIZE_STRING, 
+	'email' => FILTER_SANITIZE_EMAIL,
+	'encontrou' => FILTER_SANITIZE_STRING,
+	'assunto' => FILTER_SANITIZE_STRING,
+	'mensagem' => FILTER_SANITIZE_STRING
+);
+$revised = filter_var_array($_POST, $postfilter);
+
+$msg = "<strong>Nome: </strong>" . $revised["nome"] . "<br />";
+$msg .= "<strong>E-mail: </strong>" . $revised["email"] . "<br />";
+$msg .= "<strong>Como encontrou o site: </strong>" . $revised["encontrou"] . "<br />";
+$msg .= "<strong>Assunto: </strong>" . $revised["assunto"] . "<br />";
+$msg .= "<strong>Mensagem: </strong>" . $revised["mensagem"] . "<br />";
 
 $mensagem = $msg;
-$remetente = $email;
+$remetente = $revised["email"];
 $destinatario = "contato.TurmaNT@zerocool.com.br";
-$assunto = "Canoagem - " . $assunto;
+$assunto = "Canoagem - " . $revised["assunto"];
 
 $headers = "MIME-Version: 1.1\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
